16a637de40759370fabbde171d135b3e575f05f1
Volodymyr Tkach <vovantkac5@gmail.com>
2023-06-25 21:17:29 +0300

    Create sample tests

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator function
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
