ff691e14a1e8aeba010d2d6306e40b8c15aabcb4
Volodymyr Tkach <vovantkac5@gmail.com>
2023-06-27 21:44:10 +0300

    Configure POSTGRESQL and start core app for setting project etc

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,6 +7,8 @@ def add(x, y):
     """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
+
---
+++ my_oop_test.py
@@ -0,0 +1,5 @@
+print("2")
+a = 2
+a = str(a)
+a = [1,2,2]
+print(a)
