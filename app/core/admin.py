0c7090affb825701cf0ec89ca2bb698a0f25771e
Volodymyr Tkach <vovantkac5@gmail.com>
2023-07-06 10:39:20 +0300

    fix tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -14,6 +14,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,7 +12,7 @@ CREATE_USER_URL = reverse('user:create')
 
 
 def create_user(**params):
-    """Create and return new user."""
+    """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
 
 
@@ -52,11 +52,11 @@ class PublicUserApiTest(TestCase):
         payload = {
             'email': 'test@example.com',
             'password': 'pw',
-            'name': 'Test Name',
+            'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exist = get_user_model().objects.filter(
             email=payload['email']
-        ).exist()
+        ).exists()
         self.assertFalse(user_exist)
