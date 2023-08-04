2e205eaeac16ae869350d466c53ac2880d5d9193
Volodymyr Tkach <vovantkac5@gmail.com>
2023-08-04 17:01:42 +0300

    add listing tag serializer, view

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -14,7 +14,7 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
-def create_user(email='user@example.com', password='testpass123')
+def create_user(email='user@example.com', password='testpass123'):
     """Create and return a user."""
     return get_user_model().objects.create_user(email=email, password=password)
 
