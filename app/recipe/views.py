919a04197124fb29a1503e01ae5c63ead0f36edd
Volodymyr Tkach <vovantkac5@gmail.com>
2023-08-04 17:08:33 +0300

    add update to tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.ListModelMixin,
+                 mixins.UpdateModelMixin,
+                 viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
